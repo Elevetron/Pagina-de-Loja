c5c448beb70a13d021772a717c55e60876b6fc64
MatHennel <matheushennel123@gmail.com>
2023-11-07 22:40:31 -0300

    concluído a listagem dos produtos com as imagens

--- view/index.php
+++ view/index.php
@@ -1,24 +1,8 @@
 <?php
 include "../model/db/conecta.php";
 $sql = "SELECT * FROM PRODUTOS";
-$rs = mysqli_query($conexao,$sql);
+$rs = mysqli_query($conexao, $sql);
 $total_registros = mysqli_num_rows($rs);
-
-if (isset($_GET['id'])) {
-    $id = $_GET['id'];
-    $sql = "SELECT imagem FROM produtos WHERE id = $id";
-    $result = mysqli_query($conexao, $sql);
-
-    if ($result && mysqli_num_rows($result) > 0) {
-        $imagem = mysqli_fetch_assoc($result)['imagem'];
-        header('Content-Type: image/jpeg');
-        echo base64_decode($imagem);
-        exit;
-    }
-}
-
-
-
 ?>
 
 <!DOCTYPE html>
@@ -37,38 +21,50 @@ if (isset($_GET['id'])) {
             <div class="menu">
                 <div id="logo">
                     <img src="../images/logo_empresa.jpg" alt="Logo da Empresa" width="100" height="100">
-                
+
                 </div>
                 <a href="../view/produto/create.php" style="text-decoration: none; color: inherit;">Cadastrar Produtos</a>
                 <a href="../view/produto/read.php" style="text-decoration: none; color: inherit;">Consulta de Produtos</a>
             </div>
         </nav>
-        
-            <div class="titulo">
-                <h1>Elevetron Segurança</h1>
-            </div>
-           
 
-            <div class="produtos">
-                <h1>Equipamentos de Segurança</h1>
-                <?php
-                        while ($reg = mysqli_fetch_array($rs)){
-                            $id = $reg["id"];
-                            $descricao = $reg["descricao"];
-                            $preco = $reg["preco"];
-                            $preco = str_replace('.', ',', $preco);
-                            
-                       
-                    ?>
-                <div class="product">
-                <img src="exibicao_imagem.php?id=<?php echo $id; ?>" alt="<?php echo $descricao; ?>">
-                    <p><?php print $descricao; ?></p>
-                    <p class="price">R$ <?php print  $preco; ?></p>
-                    <button>Comprar</button>
-                </div>
-                <?php } ?>
-               
-            </div>
+        <div class="titulo">
+            <h1>Elevetron Segurança</h1>
+        </div>
+
+
+        <div class="produtos">
+    <h1>Equipamentos de Segurança</h1>
+    <?php
+    $rs_produtos = mysqli_query($conexao, $sql);
+    while ($reg = mysqli_fetch_array($rs_produtos)) {
+        $id = $reg["id"];
+        $descricao = $reg["descricao"];
+        $preco = str_replace('.', ',', $reg["preco"]);
+        $id_imagem = $reg['id_imagem'];
+
+        $rs_imagem = mysqli_query($conexao, "SELECT path FROM arquivos WHERE id = $id_imagem") or die("Erro na consulta: " . mysqli_error($conexao));
+        
+        if ($rs_imagem && mysqli_num_rows($rs_imagem) > 0) {
+            $row = mysqli_fetch_assoc($rs_imagem);
+            $path = $row["path"];
+            if (!file_exists("../$path")) {
+                $path = "images/imagens_padrao/sem_imagem.png";
+            }
+        } else {
+            $path = "images/imagens_padrao/sem_imagem.png";
+        }
+    ?>
+        <div class="product">
+            <img src="<?php echo "../$path"; ?>" alt="<?php echo $descricao; ?>">
+            <p><?php echo $descricao; ?></p>
+            <p class="price">R$ <?php echo $preco; ?></p>
+            <button>Comprar</button>
+        </div>
+    <?php } ?>
+
+</div>
+
     </body>
 
 </html>
